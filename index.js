1a383fec597fea1b957dd9579da838675cea0aee
Tyler Long <tyler4long@gmail.com>
2017-09-12 17:34:28 +0800

    Format code

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto('https://mermaidjs.github.io/mermaid-live-editor');
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage()
+  await page.goto('https://mermaidjs.github.io/mermaid-live-editor')
   await page.click('button')
   const clip = await page.$eval('svg', svg => {
-      const react = svg.getBoundingClientRect()
-      return { x: react.left, y: react.top, width: react.width, height: react.height }
+    const react = svg.getBoundingClientRect()
+    return { x: react.left, y: react.top, width: react.width, height: react.height }
   })
-  await page.screenshot({ path: 'flowchart.png', clip, omitBackground: true });
-  browser.close();
-})();
+  await page.screenshot({ path: 'flowchart.png', clip, omitBackground: true })
+  browser.close()
+})()
