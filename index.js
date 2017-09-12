c6d308aa080c54fd72d579678519aecded56ce6c
Tyler Long <tyler4long@gmail.com>
2017-09-12 18:18:45 +0800

    Generate SVG from local content

--- index.js
+++ index.js
@@ -1,17 +1,12 @@
 const puppeteer = require('puppeteer')
-const fs = require('fs');
+const fs = require('fs')
+const path = require('path');
 
 (async () => {
   const browser = await puppeteer.launch()
   const page = await browser.newPage()
-  await page.goto('https://mermaidjs.github.io/mermaid-live-editor')
-  await page.click('button')
-  const clip = await page.$eval('svg', svg => {
-    const react = svg.getBoundingClientRect()
-    return { x: react.left, y: react.top, width: react.width, height: react.height }
-  })
+  await page.goto(`file://${path.join(__dirname, 'index.html')}`)
   const svg = await page.$eval('svg', svg => svg.outerHTML)
   fs.writeFileSync('flowchart.svg', svg)
-  await page.screenshot({ path: 'flowchart.png', clip, omitBackground: true })
   browser.close()
 })()
