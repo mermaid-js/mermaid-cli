000175092cdd43e1c49f27f43cc06de3635c3c20
Tyler Long <tyler4long@gmail.com>
2017-09-13 21:13:31 +0800

    First working mermaid-cli

---
+++ README.md
@@ -0,0 +1,61 @@
+# mermaid CLI
+
+Command-line interface for [mermaid](https://mermaidjs.github.io/).
+
+This CLI tool takes a mermaid definition file as input and generates svg/png file as output.
+
+
+## Installation
+
+```
+yarn global add mermaid-cli
+```
+
+ Or
+
+```
+npm install -g mermaid-cli
+```
+
+Please install via `npm` instead of `yarn` if you encouner [this issue](https://github.com/yarnpkg/yarn/issues/2224).
+
+
+## Examples
+
+```
+mmd -i input.mmd -o output.svg
+```
+
+```
+mmd -i input.mmd -o output.png
+```
+
+```
+mmd -i input.mmd -o output.svg -w 1024 -H 768
+```
+
+```
+mmd -i input.mmd -t forest
+```
+
+
+## Options
+
+Please run the following command to see the latest options:
+
+```
+mmd -h
+```
+
+The following is for your quick reference (may not be the latest version):
+
+```
+  Options:
+    -V, --version          output the version number
+    -t, --theme [name]     Theme of the chart. Optional. Default: default
+    -w, --width [width]    Width of the page. Optional. Default: 800
+    -H, --height [height]  Height of the page. Optional. Default: 600
+    -i, --input <input>    * Input mermaid file. Required.
+    -o, --output [output]  Output image file. Optional. Default: input + ".svg"
+    -h, --help             output usage information
+```
--- index.js
+++ index.js
@@ -1,12 +1,59 @@
-const puppeteer = require('puppeteer')
+const commander = require('commander')
+const chalk = require('chalk')
 const fs = require('fs')
-const path = require('path');
+const path = require('path')
+const puppeteer = require('puppeteer')
 
-(async () => {
+const pkg = require('./package.json')
+
+const error = (message) => {
+  console.log(chalk.red(`\n${message}\n`))
+  process.exit(1)
+}
+
+commander
+  .version(pkg.version)
+  .option('-t, --theme [name]', 'Theme of the chart. Optional. Default: default', /^default|forest|dark|neutral$/, 'default')
+  .option('-w, --width [width]', 'Width of the page. Optional. Default: 800', /^\d+$/, '800')
+  .option('-H, --height [height]', 'Height of the page. Optional. Default: 600', /^\d+$/, '600')
+  .option('-i, --input <input>', chalk.blue('* Input mermaid file. Required.'))
+  .option('-o, --output [output]', 'Output image file. Optional. Default: input + ".svg"')
+  .parse(process.argv)
+
+let { theme, width, height, input, output } = commander
+
+// check input file
+if (!input) {
+  error('Please specify input file: -i <input>')
+}
+if (!fs.existsSync(input)) {
+  error(`Input file "${input}" doesn't exist`)
+}
+
+// check output file
+if (!output) {
+  output = input + '.svg'
+}
+if (!/\.(?:svg|png)$/.test(output)) {
+  error(`Output file must end with ".svg" or ".png"`)
+}
+const outputDir = path.dirname(output)
+if (!fs.existsSync(outputDir)) {
+  error(`Output directory "${outputDir}/" doesn't exist`)
+}
+
+// normalize args
+width = parseInt(width)
+height = parseInt(height)
+
+;(async () => {
   const browser = await puppeteer.launch()
   const page = await browser.newPage()
+  page.setViewport({ width, height })
   await page.goto(`file://${path.join(__dirname, 'index.html')}`)
   const svg = await page.$eval('svg', svg => svg.outerHTML)
-  fs.writeFileSync('flowchart.svg', svg)
+  fs.writeFileSync(output, svg)
   browser.close()
 })()
+
+// todo: take advantages of theme and input
