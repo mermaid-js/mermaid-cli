7324708b5d66746b55b55b6bde927eef43602750
Mindaugas Laganeckas <Mindaugas.Laganeckas@gmail.com>
2020-12-10 10:49:02 +0100

    Update run-tests.sh

--- run-tests.sh
+++ run-tests.sh
@@ -4,7 +4,6 @@ IMAGETAG=$2
 
 # Test if the CLI actually works (PNG)
 for i in $(ls $INPUT_DATA/*.mmd); do docker run -v $(pwd):/data $IMAGETAG -i /data/$i -o /data/$i.png -w 800; done
-for i in $(ls $INPUT_DATA/*.mmd); do docker run -v $(pwd):/data $IMAGETAG -i /data/$i -o /data/$i.svg -w 800; done
 for i in $(ls $INPUT_DATA/*.mmd); do cat $i | docker run -i -v $(pwd):/data $IMAGETAG -o /data/$i-stdin.png -w 800; done
 
 # Test if the CLI actually works (PDF)
