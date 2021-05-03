134633993f830fbd71e9312ad24dda756e9c4ac8
Niclas Gustafsson <niclas@bitfront.se>
2021-05-04 00:11:47 +0200

    * Added a test with additional CSS * Reworked run-tests.sh to use CSS if it finds one alongside the test .mmd file.

--- run-tests.sh
+++ run-tests.sh
@@ -2,10 +2,27 @@
 INPUT_DATA=$1
 IMAGETAG=$2
 
-# Test if the CLI actually works (PNG)
-for i in $(ls $INPUT_DATA/*.mmd); do docker run -v $(pwd):/data $IMAGETAG -i /data/$i -o /data/$i.png -w 800; done
-for i in $(ls $INPUT_DATA/*.mmd); do cat $i | docker run -i -v $(pwd):/data $IMAGETAG -o /data/$i-stdin.png -w 800; done
+# Test if the CLI actually works (PNG, PDF and SVG)
+# If there is a corresponding .css for a test, it will be used as an additional parameter: -C <css-file>
 
-# Test if the CLI actually works (PDF)
-for i in $(ls $INPUT_DATA/*.mmd); do docker run -v $(pwd):/data $IMAGETAG -i /data/$i -o /data/$i.pdf; done
-for i in $(ls $INPUT_DATA/*.mmd); do cat $i | docker run -i -v $(pwd):/data $IMAGETAG -o /data/$i-stdin.pdf; done
+for test in $(ls $INPUT_DATA/*.mmd); do
+  STYLE_PARAMS=""
+
+  STYLE=$INPUT_DATA/`basename $test .mmd`.css
+  if [ -f $STYLE ] ; then
+    STYLE_PARAMS="-C /data/$STYLE"
+  fi
+
+  for fmt in png pdf svg; do
+    EXTRA_PARAMS=""
+
+    if [ "$fmt" = "png" ] ; then
+      EXTRA_PARAMS="-w 800"
+    fi
+    echo docker run -v $(pwd):/data $IMAGETAG -i /data/$test $STYLE_PARAMS -o /data/$test.$fmt $EXTRA_PARAMS
+    docker run -v $(pwd):/data $IMAGETAG -i /data/$test $STYLE_PARAMS -o /data/$test.$fmt $EXTRA_PARAMS
+    cat $test |  docker run -i -v $(pwd):/data $IMAGETAG $STYLE_PARAMS -o /data/$test-stdin.$fmt $EXTRA_PARAMS
+
+  done
+done
+
