653a51e6a7a73e806cd04c5c772bdf78a341fd27
Mindaugas Laganeckas <Mindaugas.Laganeckas@3shape.com>
2021-05-26 11:12:03 +0200

    Remove sudo

--- setup-sandbox.sh
+++ setup-sandbox.sh
@@ -2,7 +2,7 @@
 
 # cd to the downloaded instance
 cd ./node_modules/puppeteer/.local-chromium/linux-*/chrome-linux/
-sudo chown root:root chrome_sandbox
-sudo chmod 4755 chrome_sandbox
+chown root:root chrome_sandbox
+chmod 4755 chrome_sandbox
 # copy sandbox executable to a shared location
-sudo cp -p chrome_sandbox /usr/local/sbin/chrome-devel-sandbox
+cp -p chrome_sandbox /usr/local/sbin/chrome-devel-sandbox
