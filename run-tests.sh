b281a692b509acae31cfff8301c6b09d2650b1c1
Mindaugas Laganeckas <Mindaugas.Laganeckas@3shape.com>
2021-06-25 10:45:22 +0200

    Add tests for 'PNGs rendered with docker image are missing emojis'

--- run-tests.sh
+++ run-tests.sh
@@ -11,7 +11,3 @@ for i in $(ls $INPUT_DATA/*.mmd); do cat $i | docker run -i -v $(pwd):/data $IMA
 # Test if the CLI actually works (PDF)
 for i in $(ls $INPUT_DATA/*.mmd); do docker run -v $(pwd):/data $IMAGETAG -i /data/$i -o /data/$i.pdf; done
 for i in $(ls $INPUT_DATA/*.mmd); do cat $i | docker run -i -v $(pwd):/data $IMAGETAG -o /data/$i-stdin.pdf; done
-
-# Test if the CLI actually works (SVG) for a single pathological file
-docker run -v $(pwd):/data $IMAGETAG -i /data/$INPUT_DATA/graph-with-br.mmd -o /data/$INPUT_DATA/graph-with-br.mmd.svg
-cat $INPUT_DATA/graph-with-br.mmd | docker run -i -v $(pwd):/data $IMAGETAG -o /data/$INPUT_DATA/graph-with-br.mmd-stdin.svg
