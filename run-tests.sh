d14e22c512301afdb5938b8f559445a1e2e016c7
Fabian Schlager <fabian.schlager@skyr.at>
2021-09-28 13:25:52 +0200

    Add test for #165

--- run-tests.sh
+++ run-tests.sh
@@ -11,3 +11,8 @@ for i in $(ls $INPUT_DATA/*.mmd); do cat $i | docker run -i -v $(pwd):/data $IMA
 # Test if the CLI actually works (PDF)
 for i in $(ls $INPUT_DATA/*.mmd); do docker run -v $(pwd):/data $IMAGETAG -i /data/$i -o /data/$i.pdf; done
 for i in $(ls $INPUT_DATA/*.mmd); do cat $i | docker run -i -v $(pwd):/data $IMAGETAG -o /data/$i-stdin.pdf; done
+
+# Test if mmdc crashes on Markdown files containing no mermaid charts
+OUTPUT=$(docker run -v $(pwd):/data $IMAGETAG -i /data/test-positive/no-charts.md)
+EXPECTED_OUTPUT="No mermaid charts found in Markdown input"
+[[ "$OUTPUT" == "$EXPECTED_OUTPUT" ]] || echo "Expected output to be '$EXPECTED_OUTPUT', got '$OUTPUT'"
