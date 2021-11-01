de8c32d07c6bc3fc8eab13a64507e6fb8e375c70
Mindaugas Laganeckas <Mindaugas.Laganeckas@gmail.com>
2021-11-01 09:09:57 +0100

    Update install-dependencies.sh

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -5,7 +5,7 @@ apt-get update \
                           libgconf-2-4 libgdk-pixbuf2.0-0 libglib2.0-0 \
                           libgtk-3-0 libnspr4 libpango-1.0-0 libpangocairo-1.0-0 \
                           libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 \
-                          libxcursor1 libxdamage1 libxss1 libxtst6 libappindicator1 \
+                          libxcursor1 libxdamage1 libxss1 libxtst6 \
                           libnss3 libasound2 libatk1.0-0 libc6 ca-certificates \
                           fonts-liberation2 lsb-release xdg-utils wget libgbm-dev \
                           fonts-noto-color-emoji fonts-takao
