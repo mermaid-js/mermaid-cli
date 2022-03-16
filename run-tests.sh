5d572294215310c1d42b408f75f5a06f202a67f2
mikeder <mikedernet@gmail.com>
2022-03-16 08:29:05 -0400

    fix: mac m1 docker image build

--- run-tests.sh
+++ run-tests.sh
@@ -5,24 +5,24 @@ IMAGETAG=$2
 set -e
 
 # Test if the CLI actually works (PNG)
-for i in $(ls $INPUT_DATA/*.mmd); do docker run -v $(pwd):/data $IMAGETAG -i /data/$i -o /data/$i.png -w 800; done
+for i in $(ls $INPUT_DATA/*.mmd); do docker run --rm -v $(pwd):/data $IMAGETAG -i /data/$i -o /data/$i.png -w 800; done
 
 # Test if the CLI actually works (PNG) for md files
-for i in $(ls $INPUT_DATA/*.md); do docker run -v $(pwd):/data $IMAGETAG -i /data/$i -o /data/$i.png; done
+for i in $(ls $INPUT_DATA/*.md); do docker run --rm -v $(pwd):/data $IMAGETAG -i /data/$i -o /data/$i.png; done
 
 # Test if the CLI actually works (PDF)
-for i in $(ls $INPUT_DATA/*.mmd); do docker run -v $(pwd):/data $IMAGETAG -i /data/$i -o /data/$i.pdf; done
+for i in $(ls $INPUT_DATA/*.mmd); do docker run --rm -v $(pwd):/data $IMAGETAG -i /data/$i -o /data/$i.pdf; done
 
 # Test if a diagram from STDIN can be understood
-cat $INPUT_DATA/flowchart1.mmd | docker run -i -v $(pwd):/data $IMAGETAG -o /data/$INPUT_DATA/flowchart1-stdin.png -w 800
+cat $INPUT_DATA/flowchart1.mmd | docker run --rm -i -v $(pwd):/data $IMAGETAG -o /data/$INPUT_DATA/flowchart1-stdin.png -w 800
 
 # Test if mmdc crashes on Markdown files containing no mermaid charts
-OUTPUT=$(docker run -v $(pwd):/data $IMAGETAG -i /data/test-positive/no-charts.md)
+OUTPUT=$(docker run --rm -v $(pwd):/data $IMAGETAG -i /data/test-positive/no-charts.md)
 EXPECTED_OUTPUT="No mermaid charts found in Markdown input"
 [[ "$OUTPUT" == "$EXPECTED_OUTPUT" ]] || echo "Expected output to be '$EXPECTED_OUTPUT', got '$OUTPUT'"
 
 # Test if mmdc does not replace <br> with <br/>
-docker run -v $(pwd):/data $IMAGETAG -i /data/test-positive/graph-with-br.mmd -w 800;
+docker run --rm -v $(pwd):/data $IMAGETAG -i /data/test-positive/graph-with-br.mmd -w 800;
 if grep -q "<br>" "./test-positive/graph-with-br.mmd.svg"; then
   echo "<br> has not been replaced with <br/>";
   exit 1;
