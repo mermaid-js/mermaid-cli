mermaid-js/mermaid-cli

commit e38fd34e4eb3b0fd4149b6348aabd9b72c50ee91

Allow output paths to contain dots
Change the regex to only find a '.' if it is followed by md or svg, so
as to be confident that the dot was part of the extension and not
earlier in the path.

The way the regex was written to increment the -n part of the SVG
filename it would incorrectly put said number after the first dot
encountered in the output path and crash the program.

src/index.js
@@ -228,7 +228,7 @@ const parseMMD = async (browser, definition, output) => {
       //     I.e. if "out.png", use "out-1.png", "out-2.png", etc
       //   If it is an output `.md` file, use that to base .svg numbered diagrams on
       //     I.e. if "out.md". use "out-1.svg", "out-2.svg", etc
-      const outputFile = output.replace(/(\..*)$/,`-${diagrams.length + 1}$1`).replace(/(\.md)$/, '.svg');
+      const outputFile = output.replace(/(\.(md|png))$/,`-${diagrams.length + 1}$1`).replace(/(\.md)$/, '.svg');
       const outputFileRelative = `./${path.relative(path.dirname(path.resolve(output)), path.resolve(outputFile))}`;
       diagrams.push([outputFile, md]);
       return `![diagram](${outputFileRelative})`;

commit 16e7eea32c7f546e59ceb623189ce7ed37d9d324

fix: support svg/pdf template files for md input
Currently, Mermaid only supports using png as a output when using
markdown as an input, e.g. `-i markdown.md -o hi.png` will output:
`hi-1.png`, `hi-2.png`, ...

This PR adds support for svg and pdf.

This means that when running `-i markdown.md` with no `-o` output arg,
the default `.svg` will be used to output
`markdown.md-1.svg`, `markdown.md-2.svg`, ...

Fixes: e38fd34e4eb3b0fd4149b6348aabd9b72c50ee91

src/index.js
@@ -236,7 +236,7 @@ const parseMMD = async (browser, definition, output) => {
       //     I.e. if "out.png", use "out-1.png", "out-2.png", etc
       //   If it is an output `.md` file, use that to base .svg numbered diagrams on
       //     I.e. if "out.md". use "out-1.svg", "out-2.svg", etc
-      const outputFile = output.replace(/(\.(md|png))$/,`-${diagrams.length + 1}$1`).replace(/(\.md)$/, '.svg');
+      const outputFile = output.replace(/(\.(md|png|svg|pdf))$/,`-${diagrams.length + 1}$1`).replace(/(\.md)$/, '.svg');
       const outputFileRelative = `./${path.relative(path.dirname(path.resolve(output)), path.resolve(outputFile))}`;
       diagrams.push([outputFile, md]);
       return `![diagram](${outputFileRelative})`;

src-test/test.js
@@ -132,4 +132,18 @@ describe("mermaid-cli", () => {
       compileDiagram("test-negative", "invalid.expect-error.mmd", "svg")
     ).rejects.toThrow("Parse error on line 2");
   });
+
+  test('should write multiple SVGs for default .md input by default', async () => {
+    const expectedOutputFiles = [1, 2, 3].map((i) => join('test-positive', `mermaid.md-${i}.svg`))
+    // delete any files from previous test (fs.rm added in Node v14.14.0)
+    await Promise.all(expectedOutputFiles.map((file) => fs.rm(file, { force: true })))
+
+    await promisify(execFile)('node', ['src/index.js', '-i', 'test-positive/mermaid.md'])
+
+    // files should exist, and they should be SVGs
+    await Promise.all(expectedOutputFiles.map(async (file) => {
+      const svgFile = await fs.readFile(file, { encoding: 'utf8' })
+      expect(svgFile).toMatch(/^<svg/)
+    }))
+  })
 });