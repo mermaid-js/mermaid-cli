mermaid-js/mermaid-cli

commit 9d7b4b29d3dfcaa21f4639314aadf09fe4ccbe19

fix: detect spaced codeblocks (#317 #342)
* detect indented mermaid codeblocks in MD (#317)
* detect space suffixed mermaid codeblocks in MD (#342)

Signed-off-by: Andreas Ulm <andreas.ulm@skaylink.com>

src/index.js
@@ -269,7 +269,7 @@ async function run (input, output, { puppeteerConfig = {}, quiet = false, output
     }
   }
 
-  const mermaidChartsInMarkdown = /^```(?:mermaid)(\r?\n([\s\S]*?))```$/
+  const mermaidChartsInMarkdown = /^\s*```(?:mermaid)(\r?\n([\s\S]*?))```\s*$/
   const mermaidChartsInMarkdownRegexGlobal = new RegExp(mermaidChartsInMarkdown, 'gm')
   const mermaidChartsInMarkdownRegex = new RegExp(mermaidChartsInMarkdown)
   const browser = await puppeteer.launch(puppeteerConfig)

src-test/test.js
@@ -165,7 +165,7 @@ describe('mermaid-cli', () => {
   }, timeout)
 
   test('should write multiple SVGs for default .md input by default', async () => {
-    const expectedOutputFiles = [1, 2, 3].map((i) => join('test-positive', `mermaid.md-${i}.svg`))
+    const expectedOutputFiles = [1, 2, 3, 10, 11].map((i) => join('test-positive', `mermaid.md-${i}.svg`))
     // delete any files from previous test (fs.rm added in Node v14.14.0)
     await Promise.all(expectedOutputFiles.map((file) => fs.rm(file, { force: true })))
 

commit 4f58d6c4452c81fb8b4b0c10e78fc1917ad275df

fix: handle empty newlines before diagrams in md
Handles empty newlines before and after diagrams in Markdown.

For example, the following Markdown:

````markdown
My text.

```mermaid
  my diagram
```
````

Should get converted to:

```markdown
My text.

![diagram](./my-diagram)
```

However, currently, the empty newline is removed.
We can fix this by making sure that our Regex ignores newlines outside
of the Mermaid diagram code.

Fixes: 9d7b4b29d3dfcaa21f4639314aadf09fe4ccbe19
Fixes: https://github.com/mermaid-js/mermaid-cli/issues/413

src/index.js
@@ -364,7 +364,8 @@ async function run (input, output, { puppeteerConfig = {}, quiet = false, output
     }
   }
 
-  const mermaidChartsInMarkdown = /^\s*```(?:mermaid)(\r?\n([\s\S]*?))```\s*$/
+  // TODO: should we use a Markdown parser like remark instead of rolling our own parser?
+  const mermaidChartsInMarkdown = /^[^\S\n]*```(?:mermaid)(\r?\n([\s\S]*?))```[^\S\n]*$/
   const mermaidChartsInMarkdownRegexGlobal = new RegExp(mermaidChartsInMarkdown, 'gm')
   const browser = await puppeteer.launch(puppeteerConfig)
   try {

src-test/test.js
@@ -316,6 +316,9 @@ describe("NodeJS API (import ... from '@mermaid-js/mermaid-cli')", () => {
         '(./mermaid-run-output-test-png-8.png "State diagram example with \\\\\\"double-quotes\\"")')
       expect(markdownFile).toContain(markdownImageWithCustomTitle)
 
+      // check whether newlines before/after mermaid diagram are kept
+      expect(markdownFile).toContain('There should be an empty newline after this line, but before the Mermaid diagram:\n\n')
+
       // files should exist, and they should be PNGs
       await Promise.all(expectedOutputPngs.map(async (expectedOutputPng) => {
         // markdown file should point to png relative to md file