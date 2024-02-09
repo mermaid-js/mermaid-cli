95916a29c55fe3ff4b5d784dda940855ba933d74
Yufan You <ouuansteve@gmail.com>
2024-02-09 15:51:56 +0800

    test: add test for svgId option

--- src-test/test.js
+++ src-test/test.js
@@ -268,6 +268,14 @@ describe('mermaid-cli', () => {
     expectBytesAreFormat(await fs.readFile(expectedOutputFile), 'png')
   }, timeout)
 
+  test('the id of <svg> can be set', async () => {
+    const outputFile = 'test-positive/flowchart1.mmd.svg'
+    await fs.rm(outputFile, { force: true })
+    await promisify(execFile)('node', ['src/cli.js', '-i', 'test-positive/flowchart1.mmd', '-o', outputFile, '-I', 'custom-id'])
+
+    expect((await fs.readFile(outputFile)).toString()).toMatch(/^<svg[^>]+id="custom-id"/)
+  }, timeout)
+
   test.concurrent.each(['svg', 'png', 'pdf'])('should set red background to %s', async (format) => {
     await promisify(execFile)('node', [
       'src/cli.js', '-i', 'test-positive/flowchart1.mmd', '-o', `test-output/flowchart1-red-background.${format}`,
