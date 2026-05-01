f0c215ad3297ebafad4261fe5d3259461157a37e
Alois Klink <alois@aloisklink.com>
2026-05-01 20:11:21 +0900

    ci: test using Puppeteer v23 in CI
    Add a `./scripts/install-minimum.js` file that will install the minimum
    version of all `dependencies`/`peerDependencies`. This will make sure
    that we continue to support Puppeteer v23, and that all of our
    dependency ranges in our `package.json` file remain valid.

---
+++ scripts/install-minimum.js
@@ -0,0 +1,45 @@
+#!/usr/bin/env node
+import { readFile, writeFile } from 'node:fs/promises'
+import { execFileSync } from 'child_process'
+import semver from 'semver'
+
+/**
+ * Installs the minimum dependencies of this package for testing.
+ *
+ * @TODO We might be able to switch to using PNPM's `resolutionMode=lowest-direct`
+ *       for this later.
+ */
+async function main () {
+  const packageJson = JSON.parse(await readFile('package.json', 'utf8'))
+
+  for (const dependencyType of ['dependencies', 'peerDependencies']) {
+    for (const [packageName, versionRange] of Object.entries(packageJson[dependencyType] || {})) {
+      const minimumVersion = semver.minVersion(versionRange)
+      if (!minimumVersion) {
+        throw new Error(`Could not determine minimum version for ${packageName} with version range ${versionRange}`)
+      }
+      packageJson[dependencyType][packageName] = minimumVersion.version
+    }
+  }
+
+  // Remove all optional dependencies, since they're supposed to be optional
+  packageJson.optionalDependencies = {}
+
+  await writeFile('package.json', JSON.stringify(packageJson, null, 2) + '\n', 'utf8')
+
+  const command = /** @type {const} */ ([
+    'npm',
+    'install'
+  ])
+
+  console.log(`Running command: ${command.join(' ')}`)
+
+  await execFileSync(command[0], command.slice(1), {
+    stdio: 'inherit'
+  })
+}
+
+main().catch(error => {
+  console.error(error)
+  process.exit(1)
+})
