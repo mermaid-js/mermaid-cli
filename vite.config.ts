c5b38f9708929cec50df9cbb716935cd9b381f70
Alois Klink <alois@aloisklink.com>
2026-05-14 03:47:29 +0900

    refactor: remove `<script>` from `index.html`
    This is no longer needed now that we inject all JavaScript/CSS/Fonts at
    runtime. In the future, we can probably even remove this `index.html`
    file.

--- vite.config.ts
+++ vite.config.ts
@@ -2,27 +2,4 @@ import { defineConfig } from "vite"
 
 export default defineConfig({
   base: './',
-  plugins: [
-    {
-      name: 'IIFE-converter',
-      config(currentConfig, _unused) {
-        return {
-          ...currentConfig,
-          build: {
-            ...currentConfig.build,
-            rollupOptions: {
-              ...currentConfig.build?.rollupOptions,
-              output: {
-                ...currentConfig.build?.rollupOptions?.output,
-                format: 'iife',
-              }
-            }
-          }
-        };
-      },
-      transformIndexHtml(html) {
-        return html.replace('<script type="module" crossorigin', '<script charset="utf-8"')
-      }
-    }
-  ]
 });
